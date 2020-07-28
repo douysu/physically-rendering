b7e3501d938424a1ec0315592b498d05da69f11c
douysu <yindou97@163.com>
2020-07-28 18:20:26 +0800

    [init] PBR

---
+++ physically-rendering/pbr.fs
@@ -0,0 +1,22 @@
+#version 330 core
+out vec4 FragColor;
+in vec2 TexCoords;
+in vec3 WorldPos;
+in vec3 Normal;
+
+// material parameters
+uniform vec3 albedo;
+uniform float metallic;
+uniform float roughness;
+uniform float ao;
+
+// lights
+uniform vec3 lightPositions[4];
+uniform vec3 lightColors[4];
+
+uniform vec3 camPos;
+
+void main()
+{		
+    FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+}
